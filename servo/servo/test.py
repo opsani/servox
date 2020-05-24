ccbcc55f16c67d11a50aa28825dfd10abb0fb62d
Blake Watters <blake@opsani.com>
2020-05-24 13:03:13 -0700

    Wire JSON schema mixin

--- servo/servo/test.py
+++ servo/servo/test.py
@@ -10,13 +10,43 @@ def command(*args, **kwargs):
                 self.fn = fn
 
             def __set_name__(self, owner, name):
+                setattr(owner, name, self.fn)
                 owner._typer.command(*args, **kwargs)(self.fn)
         return Command(fn)
     return wrapper
 
-class JSONSchemaMixin:
-    pass
+def callback(*args, **kwargs):
+    def wrapper(fn):
+        class Callback:
+            def __init__(self, fn):
+                self.fn = fn
 
+            def __set_name__(self, owner, name):
+                owner._typer.callback(*args, **kwargs)(self.fn)
+        return Callback(fn)
+    return wrapper
+
+def jsonschema(cls):
+    def schema(self):
+        """
+        Display the JSON Schema 
+        """
+        pass
+
+    def validate(file: typer.FileText = typer.Argument(...)):
+        """
+        Validate given file against the JSON Schema
+        """
+        pass
+    
+    # Attach the methods and directly invoke Typer decorator
+    setattr(cls, 'schema', schema)
+    cls._typer.command()(schema)
+    setattr(cls, 'validate', validate)
+    cls._typer.command()(validate)
+    return cls
+
+@jsonschema
 class Connector():
     name: ClassVar[str] = "foo" # TODO: Need to figure out the name factoring
     _typer: ClassVar[typer.Typer] = typer.Typer(name=name)
@@ -26,7 +56,7 @@ class Connector():
         print("another")
 
 class VegetaConnector(Connector):
-    # name = "vegeta"
+    name = "vegeta"
     # typer = typer.Typer(name=name)
 
     @command
