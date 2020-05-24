66e0dde543a5500ede5988b47fb849823d106e63
Blake Watters <blake@opsani.com>
2020-05-24 12:26:44 -0700

    Snapshot

--- servo/servo/test.py
+++ servo/servo/test.py
@@ -3,60 +3,62 @@ import inspect
 import sys
 from typing import ClassVar
 
-_registry = {}
+# _registry = {}
 
-def register_class(cls):
-    _registry[cls.__name__] = cls
+# def register_class(cls):
+#     _registry[cls.__name__] = cls
 
-class MyMeta(type):
-    # def __new__(self,name,base,ns):
-    #     print("meta new ", self)
-    #     return type.__new__(self,name,base,ns)
+# class MyMeta(type):
+#     # def __new__(self,name,base,ns):
+#     #     print("meta new ", self)
+#     #     return type.__new__(self,name,base,ns)
 
-    def __init__(self,name,base,ns):
-        print("meta init ", self)
-        register_class(self)
-        self.command = typer.Typer
-        type.__init__(self,name,base,ns)
+#     def __init__(self,name,base,ns):
+#         print("meta init ", self)
+#         register_class(self)
+#         self.command = typer.Typer
+#         type.__init__(self,name,base,ns)
 
-def command():    
-    def decorator(f):
-        print(f)
-        print(f.__globals__)
-        print(f.__qualname__)
-        # c = f.__globals__[f.__qualname__.rsplit('.', 1)[0]]    
-        # c = getattr(inspect.getmodule(f), f.__qualname__.rsplit('.', 1)[0])
-        name = f.__qualname__.rsplit('.', 1)[0]
-        print("NAME: ", name)
-        # cls = eval(name)
-        # print(cls)
-        print('-------')
-        print(_registry)
-        cls = _registry[name]
-        print(cls)
-        # if cls.cli == None:
-        #     cls.cli = typer.Typer()
-        return cls.command.command(f)
-    return decorator
+# def command():    
+#     def decorator(f):
+#         print(f)
+#         print(f.__globals__)
+#         print(f.__qualname__)
+#         # c = f.__globals__[f.__qualname__.rsplit('.', 1)[0]]    
+#         # c = getattr(inspect.getmodule(f), f.__qualname__.rsplit('.', 1)[0])
+#         name = f.__qualname__.rsplit('.', 1)[0]
+#         print("NAME: ", name)
+#         # cls = eval(name)
+#         # print(cls)
+#         print('-------')
+#         print(_registry)
+#         cls = _registry[name]
+#         print(cls)
+#         # if cls.cli == None:
+#         #     cls.cli = typer.Typer()
+#         return cls.command.command(f)
+#     return decorator
 
-class class_decorator:
-    def __init__(self, fn):
-        self.fn = fn
+def command(fn):
+    print(f"command invoked for {fn}")
+    class Command:
+        def __init__(self, fn):
+            self.fn = fn
 
-    def __set_name__(self, owner, name):
-        # do something with owner, i.e.
-        print(f"decorating {self.fn} and name {name} using {owner}")
-        self.fn.class_name = owner.__name__
+        def __set_name__(self, owner, name):
+            print(f"decorating {self.fn} and name {name} using {owner}")
+            self.fn.class_name = owner.__name__
 
-        # then replace ourself with the original method
-        setattr(owner, name, self.fn)
-        print(f"Typer is {owner._typer}")
-        owner._typer.command()(self.fn)
+            # then replace ourself with the original method
+            # setattr(owner, name, self.fn)
+            # print(f"Typer is {owner._typer}")
+            owner._typer.command()(self.fn)
+    return Command(fn)
 
 class JSONSchemaMixin:
     pass
 
-class Connector(metaclass=MyMeta):
+class Connector():
     name: ClassVar[str] = "foo" # TODO: Need to figure out the name factoring
     _typer: ClassVar[typer.Typer] = typer.Typer(name=name)
     
@@ -65,16 +67,17 @@ class Connector(metaclass=MyMeta):
     #     return cls._typer.command(*args,**kwargs)
 
     # @_typer.command()
-    @class_decorator
+    @command
     def another():
-        print("adasdfdsfgd")
+        print("another")
 
 class VegetaConnector(Connector):
-    pass
-    # @command()
-    # @class_decorator
-    # def fooooo(self):
-    #     print("pppopop")
+    # name = "vegeta"
+    # typer = typer.Typer(name=name)
+
+    @command
+    def wtf():
+        print("wtf")
 
 class PrometheusConnector(Connector):
     pass
@@ -91,7 +94,7 @@ class CLI:
     
     @app.command()
     def test():
-        print("wooo")
+        print("test")
 
 if __name__ == "__main__":
     cli = CLI()
