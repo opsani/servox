a742febe80241072a703a661915b967e1f916e10
Blake Watters <blake@opsani.com>
2020-07-10 14:01:41 -0700

    Stub settings for use in tests

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -2,18 +2,26 @@ import os
 from contextlib import contextmanager
 from typing import Dict
 
-from servo.connector import Connector
+from servo.connector import Connector, ConnectorSettings
+from typing import Optional
+
+
+class StubConnectorSettings(ConnectorSettings):
+    name: Optional[str]
 
 
 class MeasureConnector(Connector):
+    settings: StubConnectorSettings
     pass
 
 
 class AdjustConnector(Connector):
+    settings: StubConnectorSettings
     pass
 
 
 class LoadgenConnector(Connector):
+    # settings: StubConnectorSettings
     pass
 
 
