746b6b87f5ead794bd9eed6bd7471923d1f291cb
Blake Watters <blake@opsani.com>
2020-07-18 01:22:56 -0700

    Factor utilities into a package

---
+++ servo/utilities/__init__.py
@@ -0,0 +1,6 @@
+from .strings import *
+from .duration_str import *
+from .pydantic import *
+from .progress import *
+from .signal_handler import *
+from .yaml import *
---
+++ servo/utilities/pydantic.py
@@ -0,0 +1,12 @@
+from typing import Type, Callable, Any
+from pydantic.validators import _VALIDATORS
+
+def prepend_pydantic_validator(type_: Type[Any], validator: Callable[[Any], Any]) -> None:
+    for _validator in _VALIDATORS:
+        if _validator[0] == type_:
+            _validator[1].insert(0, validator)
+
+def append_pydantic_validator(type_: Type[Any], validator: Callable[[Any], Any]) -> None:
+    for _validator in _VALIDATORS:
+        if _validator[0] == type_:
+            _validator[1].append(validator)
