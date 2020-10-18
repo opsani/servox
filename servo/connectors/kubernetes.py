d58b3089efd5efc8022f839d9b832298b8c82f4a
Blake Watters <blake@opsani.com>
2020-10-18 07:46:39 -0700

    Drop watch permission on namespaces resource

--- servo/connectors/kubernetes.py
+++ servo/connectors/kubernetes.py
@@ -2485,7 +2485,7 @@ STANDARD_PERMISSIONS = [
     PermissionSet(
         group="",
         resources=["namespaces"],
-        verbs=["get", "list", "watch"],
+        verbs=["get", "list"],
     ),
     PermissionSet(
         group="",
