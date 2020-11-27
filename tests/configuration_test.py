b9ef0e9e0335c7b8e34199c3624e1d67b2837125
Blake Watters <blakewatters@gmail.com>
2020-11-26 17:48:23 -0800

    Start breaking out configuration module tests

---
+++ tests/configuration_test.py
@@ -0,0 +1,22 @@
+import pathlib
+import pytest
+import servo
+import tests
+
+def test_config(servo_yaml: pathlib.Path) -> None:
+    config_model = servo.assembly._create_config_model_from_routes(
+        {
+            "adjust": tests.test_helpers.AdjustConnector,
+        }
+    )
+    config = config_model.generate()
+    servo_yaml.write_text(config.yaml())
+    debug(config.yaml())
+
+    optimizer = servo.configuration.Optimizer(
+        id="dev.opsani.com/blake-ignite",
+        token="bfcf94a6e302222eed3c73a5594badcfd53fef4b6d6a703ed32604",
+    )
+    assembly_, _, _ = servo.assembly.Assembly.assemble(
+        config_file=servo_yaml, optimizer=optimizer
+    )
