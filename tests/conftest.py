7cbc53d8bde8f65691d7438316bb9f9bcd46141b
Blake Watters <blake@opsani.com>
2020-12-14 04:25:31 -0800

    Don't tear down kind cluster when running tests under GitHub Actions

--- tests/conftest.py
+++ tests/conftest.py
@@ -401,7 +401,6 @@ async def kind(request, subprocess, kubeconfig: str, kube_context: str) -> str:
             exit_code, _, _ = await subprocess(f"kind delete cluster --name {cluster} --kubeconfig {kubeconfig}", print_output=True)
             if exit_code != 0:
                 raise RuntimeError(f"failed running minikube: exited with status code {exit_code}")
-
             await subprocess(f"kubectl config --kubeconfig {kubeconfig} use-context {kube_context}", print_output=True)
 
 # TODO: Replace this with a callable like: `kind.create(), kind.delete(), with kind.cluster() as ...`
