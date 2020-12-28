6b3f5a7ab2aa2d17a707fb16b4bf47c4d0170c00
Blake Watters <blake@opsani.com>
2020-12-28 12:46:25 -0800

    Restore results count assertion to 1

--- tests/connectors/prometheus_test.py
+++ tests/connectors/prometheus_test.py
@@ -688,7 +688,7 @@ class TestConnector:
         # here we are just verifying wiring to connector
         results = await connector.check()
         assert results
-        assert len(results) == 3
+        assert len(results) == 1
         assert results[0].id == 'check_base_url'
 
     @respx.mock
