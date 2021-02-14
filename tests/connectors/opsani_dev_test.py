960c6680d2873ac8caeb3a92701fc9bb8b09df3b
Blake Watters <blake@opsani.com>
2021-02-13 23:07:19 -0800

    Run checks instead of waiting on durations

--- tests/connectors/opsani_dev_test.py
+++ tests/connectors/opsani_dev_test.py
@@ -529,7 +529,8 @@ class TestServiceMultiport:
                                 config=checks.config,
                                 deployment=deployment,
                                 kube_port_forward=kube_port_forward,
-                                load_generator=load_generator
+                                load_generator=load_generator,
+                                checks=checks
                             )
                         else:
                             break
@@ -850,7 +851,7 @@ async def wait_for_check_to_pass(
 
     return check
 
-async def _remedy_check(id: str, *, config, deployment, kube_port_forward, load_generator) -> None:
+async def _remedy_check(id: str, *, config, deployment, kube_port_forward, load_generator, checks) -> None:
     envoy_proxy_port = servo.connectors.opsani_dev.ENVOY_SIDECAR_DEFAULT_PORT
     servo.logger.warning(f"Remedying failing check '{id}'...")
     if id == 'check_deployment_annotations':
@@ -897,7 +898,12 @@ async def _remedy_check(id: str, *, config, deployment, kube_port_forward, load_
         servo.logger.critical("Step 5 - Check that traffic metrics are coming in from Envoy")
         servo.logger.info(f"Sending test traffic to Envoy through deploy/fiber-http")
         async with kube_port_forward("deploy/fiber-http", envoy_proxy_port) as envoy_url:
-            await load_generator(envoy_url).run_until(asyncio.sleep(10))
+            await load_generator(envoy_url).run_until(
+                wait_for_check_to_pass(
+                    functools.partial(checks.run_one, id=f"check_traffic_metrics")
+                )
+            )
+
 
     elif id == 'check_service_proxy':
         # Step 6
@@ -919,7 +925,11 @@ async def _remedy_check(id: str, *, config, deployment, kube_port_forward, load_
         # Step 8
         servo.logger.critical("Step 8 - Verify Service traffic makes it through Envoy and gets aggregated by Prometheus")
         async with kube_port_forward(f"service/fiber-http", 80) as service_url:
-            await load_generator(service_url).run_until(asyncio.sleep(1.0))
+            await load_generator(service_url).run_until(
+                wait_for_check_to_pass(
+                    functools.partial(checks.run_one, id=f"check_traffic_metrics")
+                )
+            )
 
     else:
         raise AssertionError(f"unhandled check: '{id}'")
