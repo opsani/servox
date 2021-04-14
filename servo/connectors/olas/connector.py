27f9b6436822fbb9def9ea71b9fedf807fe55ef1
theStrenuous <voyagersoong@gmail.com>
2021-04-13 17:29:47 -0700

    connector olas: simplify event loop

--- servo/connectors/olas/connector.py
+++ servo/connectors/olas/connector.py
@@ -1,7 +1,4 @@
-import asyncio
 import os
-import sys
-import traceback
 
 import kubernetes_asyncio
 
@@ -21,11 +18,8 @@ class OLASConnector(servo.BaseConnector):
     config: OLASConfiguration
 
     @servo.on_event()
-    def startup(self) -> None:
-        loop = asyncio.get_event_loop()
-        asyncio.set_event_loop(loop)
-
-        cfg_dict = self.config.olas
+    async def startup(self) -> None:
+        cfg_dict = self.config
         backend_dict = {'url': self.config.optimizer.url,
                         'account': self.config.optimizer.organization,
                         'app_id': self.config.optimizer.name,
@@ -34,28 +28,25 @@ class OLASConnector(servo.BaseConnector):
         servo.logger.info(f"OLAS config: {cfg_dict}")
         backend_dict['auth_token'] = self.config.optimizer.token.get_secret_value()
 
+        # FIXME: Should not be necessary because of servo.connectors.kubernetes
+        # There may be a startup dependency race (no long hanging test path atm)
+        if os.getenv("KUBERNETES_SERVICE_HOST"):
+            kubernetes_asyncio.config.load_incluster_config()
+        else:
+            await kubernetes_asyncio.config.load_kube_config()
+
         olas = controller.OLASController(cfg_dict, backend_dict)
-        try:
-            task = loop.create_task(_olas_task(olas))
-            loop.run_until_complete(task)
-        except Exception:
-            servo.logger.critical(traceback.format_exc())
-            task = loop.create_task(olas.client.upload_message(olas.ts, traceback.format_exc()))
-            loop.run_until_complete(task)
-            sys.exit(1)
+        await olas.boot()
+
+        # repeating_task uses a fixed interval yet the interval of update of K8s node metrics may
+        # vary slightly thus for the time being the sleep function is done in controller instead
+        self.start_repeating_task(
+            "OLAS control loop", 0, olas.control_task
+        )
 
     @servo.on_event()
     async def shutdown(self) -> None:
         self.cancel_repeating_tasks()
-
-
-async def _olas_task(olas):
-    if os.getenv("KUBERNETES_SERVICE_HOST"):
-        kubernetes_asyncio.config.load_incluster_config()
-    else:
-        await kubernetes_asyncio.config.load_kube_config()
-
-    await olas.control_task()
 
 
 app = servo.cli.ConnectorCLI(OLASConnector, help="Opsani Learning Autoscaler")
