e897b4d756e267a40b116825053707e19f893ed7
Fred Sharp <linkous8@gmail.com>
2021-04-14 12:29:19 -0500

    Fix canary handle error - failed adjust no longer appears successful when rollback or destroyed

--- servo/connectors/kubernetes.py
+++ servo/connectors/kubernetes.py
@@ -2830,7 +2830,8 @@ class CanaryOptimization(BaseOptimization):
                     "creating new tuning pod against baseline following failed adjust"
                 )
                 self.tuning_pod = await self.target_deployment.ensure_tuning_pod(timeout=self.timeout)
-                return True
+                
+                raise error # Always communicate errors to backend unless ignored
 
             except Exception as handler_error:
                 raise handler_error from error
