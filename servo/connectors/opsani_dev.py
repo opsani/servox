2a8a3473c7a2057c7e172a888f24d87dfb9551c7
Fred Sharp <linkous8@gmail.com>
2021-05-07 12:02:57 -0500

    Add horizontal pod autoscaler config to opsani_dev  connector

--- servo/connectors/opsani_dev.py
+++ servo/connectors/opsani_dev.py
@@ -55,6 +55,10 @@ class OpsaniDevConfiguration(servo.BaseConfiguration):
     cpu: CPU
     memory: Memory
     prometheus_base_url: str = PROMETHEUS_SIDECAR_BASE_URL
+    horizontal_pod_autoscaler: bool = pydantic.Field(
+        False,
+        description="Set to True to indicate the deployment object is controlled by a Horizontal Pod Autoscaler"
+    )
 
     @classmethod
     def generate(cls, **kwargs) -> "OpsaniDevConfiguration":
@@ -80,6 +84,7 @@ class OpsaniDevConfiguration(servo.BaseConfiguration):
             description="Update the namespace, deployment, etc. to match your Kubernetes cluster",
             deployments=[
                 servo.connectors.kubernetes.DeploymentConfiguration(
+                    horizontal_pod_autoscaler=self.horizontal_pod_autoscaler,
                     name=self.deployment,
                     strategy=servo.connectors.kubernetes.CanaryOptimizationStrategyConfiguration(
                         type=servo.connectors.kubernetes.OptimizationStrategy.canary,
