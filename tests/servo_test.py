2ef95cb9d3853c52ebaf2a9dcd7e831cd10fd152
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2021-10-08 14:45:04 -0400

    removed assertions in tests that required a specific error message having content duplicated from the source

--- tests/servo_test.py
+++ tests/servo_test.py
@@ -468,10 +468,6 @@ def test_registering_event_handler_with_missing_positional_param_fails() -> None
             pass
 
     assert error
-    assert (
-        str(error.value)
-        == """invalid event handler "adjust": missing required parameter "adjustments" in callable signature "(self) -> servo.types.Description", expected "(self, adjustments: 'List[servo.types.Adjustment]', control: 'servo.types.Control' = Control(duration=Duration('0'), delay=Duration('0'), warmup=Duration('0'), settlement=None, load=None, userdata=None, environment=None)) -> 'servo.types.Description'\""""
-    )
 
 
 def test_registering_event_handler_with_missing_keyword_param_fails() -> None:
@@ -482,10 +478,6 @@ def test_registering_event_handler_with_missing_keyword_param_fails() -> None:
             pass
 
     assert error
-    assert (
-        str(error.value)
-        == """invalid event handler "measure": missing required parameter "metrics" in callable signature "(self, *, control: servo.types.Control = Control(duration=Duration('0'), delay=Duration('0'), warmup=Duration('0'), settlement=None, load=None, userdata=None, environment=None)) -> servo.types.Measurement", expected "(self, *, metrics: 'List[str]' = None, control: 'servo.types.Control' = Control(duration=Duration('0'), delay=Duration('0'), warmup=Duration('0'), settlement=None, load=None, userdata=None, environment=None)) -> 'servo.types.Measurement'\""""
-    )
 
 
 def test_registering_event_handler_with_missing_keyword_param_succeeds_with_var_keywords() -> None:
@@ -539,10 +531,6 @@ def test_registering_before_handler_fails_with_extra_args() -> None:
             pass
 
     assert error
-    assert (
-        str(error.value)
-        == """invalid before event handler "before:measure": encountered unexpected parameters "another and invalid" in callable signature "(self, invalid: str, another: int) -> None", expected "(self, *, metrics: 'List[str]' = None, control: 'servo.types.Control' = Control(duration=Duration('0'), delay=Duration('0'), warmup=Duration('0'), settlement=None, load=None, userdata=None, environment=None)) -> 'None'\""""
-    )
 
 
 def test_validation_of_before_handlers_ignores_kwargs() -> None:
