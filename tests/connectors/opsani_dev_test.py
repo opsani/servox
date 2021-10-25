bf2f4dc89cda1779af0f9539a3aabdecbefc8b66
Fred L Sharp <linkous8@gmail.com>
2021-10-25 13:02:56 -0500

    Fix integration tests failing from renamed checks - add "kubernetes" to kubernetes test check IDs - add "opsani_dev" to opsani dev test check IDs - undo timeout changes to xfaling test test_rollout_check_annotations

--- tests/connectors/opsani_dev_test.py
+++ tests/connectors/opsani_dev_test.py
@@ -170,7 +170,7 @@ class TestIntegration:
         async def test_resource_exists(
             self, resource: str, checks: servo.connectors.opsani_dev.OpsaniDevChecks
         ) -> None:
-            result = await checks.run_one(id=f"check_kubernetes_{resource}")
+            result = await checks.run_one(id=f"check_opsani_dev_kubernetes_{resource}")
             assert result.success
 
         async def test_target_container_resources_within_limits(
@@ -371,7 +371,7 @@ class TestRolloutIntegration:
         async def test_rollout_resource_exists(
             self, resource: str, rollout_checks: servo.connectors.opsani_dev.OpsaniDevRolloutChecks
         ) -> None:
-            result = await rollout_checks.run_one(id=f"check_kubernetes_{resource}")
+            result = await rollout_checks.run_one(id=f"check_opsani_dev_kubernetes_{resource}")
             assert result.success
 
         async def test_rollout_check_rsrc_limits(
@@ -512,7 +512,7 @@ class TestRolloutIntegration:
 
             async with change_to_resource(rollout):
                 await _run_remedy_from_check(result)
-                await asyncio.wait_for(wait_for_resource_version_update(), timeout=60)
+                await asyncio.wait_for(wait_for_resource_version_update(), timeout=5)
 
             result = await rollout_checks.run_one(id=f"check_controller_annotations")
             assert result.success, f"Expected success after remedy was run but got: {result}"
@@ -566,7 +566,7 @@ class TestServiceMultiport:
     async def test_requires_port_config_when_multiple_exist(
         self, kube, checks: servo.connectors.opsani_dev.OpsaniDevChecks, multiport_service
     ) -> None:
-        result = await checks.run_one(id=f"check_kubernetes_service_port")
+        result = await checks.run_one(id=f"check_opsani_dev_kubernetes_service_port")
         assert not result.success
         assert result.exception
         assert result.message == 'caught exception (ValueError): service defines more than one port: a `port` (name or number) must be specified in the configuration'
@@ -575,7 +575,7 @@ class TestServiceMultiport:
         self, kube, checks: servo.connectors.opsani_dev.OpsaniDevChecks, multiport_service
     ) -> None:
         checks.config.port = 'elite'
-        result = await checks.run_one(id=f"check_kubernetes_service_port")
+        result = await checks.run_one(id=f"check_opsani_dev_kubernetes_service_port")
         assert result.success
         assert result.message == 'Service Port: elite 31337:31337/TCP'
 
@@ -583,7 +583,7 @@ class TestServiceMultiport:
         self, kube, checks: servo.connectors.opsani_dev.OpsaniDevChecks, multiport_service
     ) -> None:
         checks.config.port = 80
-        result = await checks.run_one(id=f"check_kubernetes_service_port")
+        result = await checks.run_one(id=f"check_opsani_dev_kubernetes_service_port")
         assert result.success
         assert result.message == 'Service Port: http 80:8480/TCP'
 
@@ -600,7 +600,7 @@ class TestServiceMultiport:
         self, kube, checks: servo.connectors.opsani_dev.OpsaniDevChecks, multiport_service
     ) -> None:
         checks.config.port = 187
-        result = await checks.run_one(id=f"check_kubernetes_service_port")
+        result = await checks.run_one(id=f"check_opsani_dev_kubernetes_service_port")
         assert not result.success
         assert result.message == 'caught exception (LookupError): could not find a port numbered: 187'
     # Errors:
