aa153f3ce6cb4d71f627f6ea7d1db5da292b683a
Linkous Sharp <linkous@appdynamics.com>
2023-02-21 17:01:12 -0600

    Initial implementation of no-op connector

---
+++ servo/connectors/wait.py
@@ -0,0 +1,53 @@
+import servo
+
+
+class WaitConfiguration(servo.BaseConfiguration):
+    measure_enabled: bool
+
+
+@servo.metadata(
+    description="Wait (no-op) Connector for Opsani",
+    version="0.0.1",
+    homepage="https://github.com/opsani/servox",
+    license=servo.License.apache2,
+    maturity=servo.Maturity.stable,
+)
+class WaitConnector(servo.BaseConnector):
+    """
+    The wait connector is a dummy no-op connector which supports externally driven operation (currently measure only)
+    by waiting for the specified duration and reporting progress in the same fashion as any other connector without
+    interacting with any external systems
+    """
+
+    config: WaitConfiguration
+
+    @servo.on_event()
+    def describe(self, control: servo.Control = servo.Control()) -> servo.Description:
+        return servo.Description(metrics=[])
+
+    @servo.on_event()
+    async def measure(
+        self, *, metrics: list[str] = None, control: servo.Control = servo.Control()
+    ) -> servo.Measurement:
+        if self.config.measure_enabled:
+            measurement_duration = servo.Duration(control.warmup + control.duration)
+            self.logger.info(
+                f"Waiting for {measurement_duration} for external measurement to complete"
+            )
+
+            progress = servo.EventProgress(
+                timeout=measurement_duration, settlement=None
+            )
+            await progress.watch(self.log_progress)
+
+        return servo.Measurement(readings=[])
+
+    async def log_progress(self, progress: servo.EventProgress) -> None:
+        """Report a progress message (heart beat) to the CO api with percent of completion (also logs to stdout)"""
+        return self.logger.info(
+            progress.annotate(
+                f"Waiting for {progress.timeout} for external measurement to complete",
+                False,
+            ),
+            progress=progress.progress,
+        )
