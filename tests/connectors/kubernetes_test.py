415a7cc3275cb3c92ea7a14ed228a62e1aceb23e
Linkous Sharp <linkous@cisco.com>
2023-12-21 15:07:09 -0600

    Fix regex pattern typo

--- tests/connectors/kubernetes_test.py
+++ tests/connectors/kubernetes_test.py
@@ -1479,7 +1479,7 @@ class TestKubernetesConnectorIntegration:
                 re.escape(
                     "Requested adjustment(s) (fiber-http/fiber-http-tuning.mem=128Gi, fiber-http/fiber-http-tuning.cpu=100) cannot be scheduled due to "
                 )
-                + r"\"\d+/\d+ nodes are available:.* \d+ Insufficient cpu\.* \d+ Insufficient memory.*\""
+                + r"\"\d+/\d+ nodes are available:.* \d+ Insufficient cpu.* \d+ Insufficient memory.*\""
             ),
         ) as rejection_info:
             await connector.adjust(adjustments)
